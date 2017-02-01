77455d1ffde13b0b804def58308425f62b3bfcda
Rohit Sehgal <rohitsehgal1994@gmail.com>
2017-02-01 23:08:30 +0530

    Main server Logic

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080 ;
+
+app.set('view engine','ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',function(req,res)){
+	res.render('index');
+});
+
+app.listen(port,function(){
+	console.log('App Running In in node'+port);
+});
+
