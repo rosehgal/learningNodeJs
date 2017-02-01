e43305dcbedb2bf380160b0d438b9c4790a00791
Rohit Sehgal <rohitsehgal1994@gmail.com>
2017-02-01 23:15:29 +0530

    Error removed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 8080 ;
 app.set('view engine','ejs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/',function(req,res)){
+app.get('/',function(req,res){
 	res.render('index');
 });
 
